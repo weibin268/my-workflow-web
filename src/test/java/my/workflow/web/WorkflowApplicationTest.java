62c790e09156a3d989f71a48a524cdcd298e4839
zwb <zhuangweibin@stbl.cc>
2019-10-21 14:42:14 +0800

    aa

---
+++ src/test/java/my/workflow/web/WorkflowApplicationTest.java
@@ -0,0 +1,11 @@
+package my.workflow.web;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@SpringBootTest(classes = {WorkflowApplication.class})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class WorkflowApplicationTest {
+
+}
